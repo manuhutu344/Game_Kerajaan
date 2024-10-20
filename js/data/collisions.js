83ca62827e67e8363f1bfe411eb76e1945d325f6
manuhutu344 <nandoperdos2@gamil.com>
2024-10-20 14:09:47 +0900

    Membuat util.js

--- js/data/collisions.js
+++ js/data/collisions.js
@@ -8,14 +8,6 @@ const collisionsLevel1 = [
   0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
 ]
 
-Array.prototype.parse2D = function(){
-  const rows = []
-  for (let i = 0; i < this.length; i+=16){
-    rows.push(this.slice(i, i + 16))
-  }
-  return rows
-}
-
 const collisionsLevel2 = [
   292, 292, 292, 292, 292, 292, 292, 0, 0, 0, 0, 0, 0, 0, 0, 0, 292, 0, 0, 0, 0,
   0, 292, 0, 0, 0, 0, 0, 0, 0, 0, 0, 292, 0, 0, 0, 0, 0, 292, 0, 0, 0, 0, 0, 0,
---
+++ js/utils.js
@@ -0,0 +1,7 @@
+Array.prototype.parse2D = function(){
+    const rows = []
+    for (let i = 0; i < this.length; i+=16){
+      rows.push(this.slice(i, i + 16))
+    }
+    return rows
+  }
